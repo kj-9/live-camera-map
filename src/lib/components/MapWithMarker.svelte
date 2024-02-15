442fb821dcd42299fa280fce129fa06b02034879
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-15 21:57:55 +0900

    delete unused func

--- src/lib/components/MapWithMarker.svelte
+++ src/lib/components/MapWithMarker.svelte
@@ -7,14 +7,10 @@
 	import MapMarker from '$lib/components/map/MapMarker.svelte';
 
 	const data = getContext(contextKey);
-
-	const getIframe = (id: string, width: string, height: string) => {
-		return `<iframe width=\"${width}\" height=\"${height}\" src=\"https://www.youtube.com/embed/${id}?autoplay=1&playsinline=1\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>`;
-	};
 </script>
 
 <Map>
-	{#each $data as { position: { center }, name, org, video: { id }, title }}
+	{#each $data as { position: { center }, name, org, video: { id } }}
 		<MapMarker lon={center[0]} lat={center[1]}>
 			<h1 class="text-lg">{org}: {name}</h1>
 			<iframe
