f7b0b8423511f4766bbdc95406443b4d2dd60fb1
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-16 01:41:52 +0900

    update maker

--- src/lib/components/map/MapMarker.svelte
+++ src/lib/components/map/MapMarker.svelte
@@ -1,24 +1,42 @@
 <script lang="ts">
 	import { contextKey } from '$lib/components/map/Map.svelte';
 	import { Popup, Marker } from '$lib/maplibreGL';
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 
 	const { getMap } = getContext(contextKey);
 	const map = getMap();
 
 	export let lat;
 	export let lon;
-	let content: HTMLElement;
 
-	$: if (content) {
-		const popup = new Popup({ offset: 25 }).setDOMContent(content);
-		new Marker().setLngLat([lon, lat]).setPopup(popup).addTo(map);
-	}
+	let marker = new Marker();
+	let popup;
+	let content: HTMLElement | undefined;
+
+	onMount(() => {
+		try {
+			marker.setLngLat([lon, lat]).addTo(map);
+			popup = new Popup({ offset: 25 }).setDOMContent(content);
+			marker.setPopup(popup);
+		} catch (e) {
+			// cleanup
+			marker?.remove();
+			marker = null;
+			popup?.remove();
+			popup = null;
+			console.error(e);
+		}
+
+		content = undefined; // if successfully mounted, content is no longer needed. or it will be duplicated.
+	});
 </script>
 
-<div bind:this={content}>
-	<slot />
-</div>
+{#if $$slots.default && marker}
+	<!-- if marker is failed to created, hide slots -->
+	<div bind:this={content}>
+		<slot />
+	</div>
+{/if}
 
 <style>
 	/* override maplibre-gl's style sheet */
