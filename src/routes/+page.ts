7030ac13cecb9fe1c1e12c6a20e46d4c5464663d
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-20 22:18:41 +0900

    filtering data

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -10,13 +10,17 @@ export function load({ params }) {
 
 	// 提供されたデータをGeoJSON形式に変換する
 	data.forEach((item) => {
+		// remove items without coordinates
+		if (item.position.center.length !== 2) {
+			return;
+		}
 		const feature = {
 			type: 'Feature',
 			properties: {
 				name: item.name,
 				org: item.org,
 				id: item.video.id,
-				bearing: item.position.bearing
+				bearing: item.position.bearing || 0
 			},
 			geometry: {
 				type: 'Point',
