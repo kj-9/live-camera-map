90af849cd33c92d0a81cd94b7301556c74825ec6
Kj <46800811+kj-9@users.noreply.github.com>
2024-05-19 23:49:57 +0900

    add head tag in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.pcss';
 </script>
 
+<svelte:head>
+	<title>Live Camera Map</title>
+	<meta name="description" content="live camera map" />
+</svelte:head>
+
 <div>
 	<slot />
 </div>
