d2b52c04276f9d146bf44a6ab957ba70cfa9dd52
Kj <46800811+kj-9@users.noreply.github.com>
2024-05-19 23:49:34 +0900

    move map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,111 +1,29 @@
-<script lang="ts">
-	import * as Drawer from '$lib/components/ui/drawer';
-	import Table from '$lib/components/Table.svelte';
-	import { Button } from '$lib/components/ui/button';
-	import { Search } from 'lucide-svelte';
+<div class="container mx-auto mt-10">
+	<h1
+		class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl"
+	>
+		Live Cameras
+	</h1>
+	<p class="mb-6 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-2 lg:text-xl xl:px-4">
+		Watch live cameras from around the Japan.
+	</p>
+	<a
+		href="map"
+		class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mr-4"
+	>
+		地図で見る
+		<svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+		  </svg>
+	</a>
+	<a
+		href="table"
+		class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+	>
+		一覧で見る
+		<svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+		  </svg>
+	</a>
 
-	import { map } from '$lib/components/map/Map.svelte';
-	import iconURL from '../asset/icons8-wall-mount-camera-96.png';
-	import Map from '$lib/components/map/Map.svelte';
-	import MarkerLayer from '$lib/components/map/MarkerLayer.svelte';
-	import Popup from '$lib/components/map/Popup.svelte';
-	import { page } from '$app/stores';
-	import { pushState } from '$app/navigation';
-
-	/** @type {import('./$types').PageData} */
-	export let data;
-
-	let openPopup: boolean = false;
-	let openDrawer = false;
-
-	const FLY_DURATION = 3000;
-
-	const onClickMarker = (e) => {
-		const feature = e.features[0];
-
-		pushState('', {
-			selected: {
-				geometry: feature.geometry,
-				properties: feature.properties
-			}
-		});
-
-		if (openPopup) {
-			openPopup = false;
-		} else {
-			$map.once('moveend', () => {
-				openPopup = true;
-			});
-		}
-	};
-
-	const onClickRow = (feature) => {
-		openDrawer = false;
-		pushState('', {
-			selected: {
-				geometry: feature.geometry,
-				properties: feature.properties
-			}
-		});
-
-		if (openPopup) {
-			openPopup = false;
-		} else {
-			$map.once('moveend', () => {
-				openPopup = true;
-			});
-		}
-	};
-
-	$: {
-		const feature = $page.state.selected;
-
-		// fly to selected marker
-		if (feature && $map) {
-			$map.flyTo({
-				speed: 1,
-				essential: true,
-				center: feature.geometry.coordinates,
-				pitch: 60, // tilt, 60 is max
-				bearing: feature.properties.bearing, // bearing in degrees
-				zoom: 15
-			});
-		}
-	}
-</script>
-
-<svelte:head>
-	<title>Live Camera Map</title>
-	<meta name="description" content="live camera map" />
-</svelte:head>
-
-<Map>
-	<MarkerLayer geoJson={data} {iconURL} onClick={onClickMarker} />
-	{#if openPopup && $page.state.selected}
-		{@const {
-			properties: { id, name, org },
-			geometry: { coordinates }
-		} = $page.state.selected}
-		<Popup bind:open={openPopup} center={coordinates}>
-			<h1 class="text-lg">{org}: {name}</h1>
-			<iframe
-				class="aspect-video w-screen md:w-96"
-				title="{org}: {name}"
-				src="https://www.youtube.com/embed/{id}?autoplay=1&playsinline=1"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen
-			></iframe>
-		</Popup>
-	{/if}
-	<Drawer.Root bind:open={openDrawer}>
-		<Drawer.Trigger class="absolute bottom-5 right-5 z-10">
-			<Button>
-				<Search class="mr-2 h-4 w-4" size="20" />Search
-			</Button>
-		</Drawer.Trigger>
-		<Drawer.Content class="px-4">
-			<Table data={data.features} {onClickRow} />
-		</Drawer.Content>
-	</Drawer.Root>
-</Map>
+</div>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import * as Drawer from '$lib/components/ui/drawer';
+	import Table from '$lib/components/Table.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Search } from 'lucide-svelte';
+
+	import { map } from '$lib/components/map/Map.svelte';
+	import iconURL from '$lib/asset/icons8-wall-mount-camera-96.png';
+	import Map from '$lib/components/map/Map.svelte';
+	import MarkerLayer from '$lib/components/map/MarkerLayer.svelte';
+	import Popup from '$lib/components/map/Popup.svelte';
+	import { page } from '$app/stores';
+	import { pushState } from '$app/navigation';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	let openPopup: boolean = false;
+	let openDrawer = false;
+
+	const FLY_DURATION = 3000;
+
+	const onClickMarker = (e) => {
+		const feature = e.features[0];
+
+		pushState('', {
+			selected: {
+				geometry: feature.geometry,
+				properties: feature.properties
+			}
+		});
+
+		if (openPopup) {
+			openPopup = false;
+		} else {
+			$map.once('moveend', () => {
+				openPopup = true;
+			});
+		}
+	};
+
+	const onClickRow = (feature) => {
+		openDrawer = false;
+		pushState('', {
+			selected: {
+				geometry: feature.geometry,
+				properties: feature.properties
+			}
+		});
+
+		if (openPopup) {
+			openPopup = false;
+		} else {
+			$map.once('moveend', () => {
+				openPopup = true;
+			});
+		}
+	};
+
+	$: {
+		const feature = $page.state.selected;
+
+		// fly to selected marker
+		if (feature && $map) {
+			$map.flyTo({
+				speed: 1,
+				essential: true,
+				center: feature.geometry.coordinates,
+				pitch: 60, // tilt, 60 is max
+				bearing: feature.properties.bearing, // bearing in degrees
+				zoom: 15
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Live Camera Map</title>
+	<meta name="description" content="live camera map" />
+</svelte:head>
+
+<Map>
+	<MarkerLayer geoJson={data} {iconURL} onClick={onClickMarker} />
+	{#if openPopup && $page.state.selected}
+		{@const {
+			properties: { id, name, org },
+			geometry: { coordinates }
+		} = $page.state.selected}
+		<Popup bind:open={openPopup} center={coordinates}>
+			<h1 class="text-lg">{org}: {name}</h1>
+			<iframe
+				class="aspect-video w-screen md:w-96"
+				title="{org}: {name}"
+				src="https://www.youtube.com/embed/{id}?autoplay=1&playsinline=1"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+			></iframe>
+		</Popup>
+	{/if}
+	<Drawer.Root bind:open={openDrawer}>
+		<Drawer.Trigger class="absolute bottom-5 right-5 z-10">
+			<Button>
+				<Search class="mr-2 h-4 w-4" size="20" />Search
+			</Button>
+		</Drawer.Trigger>
+		<Drawer.Content class="px-4">
+			<Table data={data.features} {onClickRow} />
+		</Drawer.Content>
+	</Drawer.Root>
+</Map>
